d783ac1a0530da1573e0deaf393b57d9b1ba547a
VkashSah <mevkashsah@gmail.com>
2022-05-29 21:44:37 +0545

    stripe test

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
+
+function App() {
+  const makePayment = (token) => {
+    const body = {
+      token,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    return fetch(`http://localhost:3000/dev/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log(response, "Response");
+        const { status } = response;
+        console.log(status, "Status");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="App">
+      <StripeCheckout
+        stripeKey="pk_test_hysuUu3LXxL5kAImR3ayLJkb00H1CKNAE4"
+        token={makePayment}
+        name="Freight Genie"
+        planId="123"
+      >
+        <button className="btn-large blue">Subscribe Now!!!</button>
+      </StripeCheckout>
+    </div>
+  );
+}
+
+export default App;
